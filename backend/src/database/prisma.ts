96399c1bd47bcf4635a67015be2a1d5d598dd7b2
luisfilippe650 <lippe650.filippe@gmail.com>
2026-08-21 19:46:32 -0300

    chore: conexao banco de dados (MF-03)

---
+++ backend/src/database/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { env } from "../config/env";
+
+const database = new URL(env.DATABASE_URL);
+
+const adapter = new PrismaMariaDb({
+  host: database.hostname,
+  port: Number(database.port),
+  user: database.username,
+  password: database.password,
+  database: database.pathname.substring(1), // Remove the leading slash
+  allowPublicKeyRetrieval: true, // Allow public key retrieval for MariaDB
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});
